75af4422baa9fc9209b85d17042e918eeab99a44
BianchiBill <rafaelbianchi06@gmail.com>
2024-11-03 19:42:21 -0300

    chore: update

--- index.js
+++
@@ -1,6 +0,0 @@
-import { start } from './packages/fastify/index.js';
-
-( async () => {
-  await start();
-  // console.log( Object.keys( chars ) );
-})();
--- packages/fastify/app.js
+++ packages/fastify/app.js
@@ -12,7 +12,7 @@ export const start = async () => {
 
   await fastify.register( ngnl_router );
 
-  fastify.listen({ port: 3000 }, function ( err, address ) {
+  fastify.listen({ port: 8081 }, function ( err, address ) {
     if ( err ) {
       fastify.log.error( err );
       process.exit( 1 );
